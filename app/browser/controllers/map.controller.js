f59a03d96d024b502878fccb59e743d7b2e74b18
jyyeh77 <jyyeh77@gmail.com>
2016-09-07 22:34:45 -0400

    retrieved station information upon click

--- app/browser/controllers/map.controller.js
+++ app/browser/controllers/map.controller.js
@@ -1,9 +1,15 @@
-app.controller('MapCtrl', function($rootScope, $scope, NgMap) {
+app.controller('MapCtrl', function($rootScope, $scope, NgMap, StationFactory) {
 
 	NgMap.getMap().then(function(map) {
-		console.log(map.getCenter());
-		console.log('markers', map.markers);
-		console.log('shapes', map.shapes);
+		// console.log(map.getCenter());
+		// console.log('markers', map.markers);
+		// console.log('shapes', map.shapes);
 	});
+
+	$scope.getInfo = function(event, station){
+		StationFactory.setStation(station);
+		$scope.stationId = StationFactory.getStation().station_id;
+		console.log("CURRENT STATION ID", $scope.stationId);
+	}
 
 });
